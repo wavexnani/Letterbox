2690dd10969ebc80abf746f4fb3c384a29e6c73d
wavexnani <mouli7667@gmail.com>
2025-03-23 11:51:26 +0530

    updated the git status kaomoji

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,12 +5,6 @@ export default function SearchPage() {
                 <input type="text" className="absolute top-0 w-full  h-10 rounded-2xl text-black bg-white"/>
                 <button className="absolute right-0 w-fit rounded-2xl font-bold h-10 bg-amber-300 pt-1 px-6 pb-1">Search</button>
             </div> 
-            
-            <div className="">
-                <div></div>
-            </div>
-
-
         </div>
     );
 }
